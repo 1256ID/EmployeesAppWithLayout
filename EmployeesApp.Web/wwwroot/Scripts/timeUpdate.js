c1a81554d75573cc108ffc0ef41128d75b44b0b2
Hamredahl <JHamredahl@gmail.com>
2025-05-19 15:23:17 +0200

    Added timebox

---
+++ EmployeesApp.Web/wwwroot/Scripts/timeUpdate.js
@@ -0,0 +1,7 @@
+﻿function updateTimeBox() {
+    const d = new Date();
+    let time = d.toLocaleTimeString();
+    document.getElementById("timeNow").textContent = time;
+}
+updateTimeBox();
+setInterval(updateTimeBox, 1000);
